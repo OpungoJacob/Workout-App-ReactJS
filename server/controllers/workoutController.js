27b7c7a2d06a018f265281492e5dcc67922c8f4d
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-02-15 13:57:31 +0300

    new

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -1,4 +1,5 @@
 const Workout = require("../models/workoutmodel");
+const mongoose = require('mongoose')
 
 //Get all workouts
 const getWorkouts = async(req, res) =>{
@@ -10,6 +11,10 @@ const getWorkouts = async(req, res) =>{
 //Get a single workout
 const getWorkout = async(req, res) =>{
     const {id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: 'No such workout'})
+    }
 
     const workout = await Workout.findById(id)
 
