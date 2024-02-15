287826fe313278ef99e79eb3be671b926644df76
OpungoJacob <145338644+OpungoJacob@users.noreply.github.com>
2024-02-15 15:55:54 +0300

    new

--- server/controllers/workoutController.js
+++ server/controllers/workoutController.js
@@ -48,18 +48,37 @@ const deleteWorkout = async (req, res) =>{
     const workout = await Workout.findOneAndDelete({_id: id})
 
     if(!workout){
-        return res.status(404).json({error: "No such workout"})
+        return res.status(400).json({error: "No such workout"})
     }
+    res.status(200).json(workout)
 }
 
 
 
 //update a workout
+const updateWorkout = async (req, res) => {
+    const {id} = res.params
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error: "No such workout"})
+    }
+
+    const workout = await Workout.findByIdAndUpdate({_id: id}, {
+        ...req.body
+    })
+    if(!workout){
+        return res.status(400).json({error: "No such workout"})
+    }
+    res.status(200).json(workout)
+
+}
 
 
 
 module.exports = {
     getWorkouts,
     getWorkout,
+    deleteWorkout,
+    updateWorkout,
     createWorkout
 }
